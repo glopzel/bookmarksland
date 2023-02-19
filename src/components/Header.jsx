3c6db1eb27e008a9fc922f1c71f742987c45b965
glopzel <74939915+lozzel@users.noreply.github.com>
2023-02-19 16:55:05 -0300

    navbar toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,41 @@
 import logo from "../assets/images/logo-bookmark.svg"
 import burgerIcon from "../assets/images/icon-hamburger.svg"
 import closeIcon from "../assets/images/icon-close.svg"
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 
 const Header = () => {
   const [navExpanded, setNavExpanded] = useState(false)
 
-  const toggleNav = () => {
+  const toggleNav = (e) => {
     setNavExpanded(!navExpanded)
+    e.currentTarget.setAttribute('aria-expanded', !navExpanded)
   }
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const primaryNav = ref.current;
+ 
+    const navBtn = document.querySelector('.mobile-nav-toggle');
+    const attr = navBtn.getAttribute('aria-expanded')
+
+    primaryNav.setAttribute('data-visible', attr);
+
+  }, [navExpanded]);
 
   return (
     <header className="first-header">
         <div className="logo"><img src={logo} alt="" /></div>
-        <button className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded='false' onClick={() => toggleNav()}>
+        <button className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded='false' onClick={(e) => toggleNav(e)}>
           <img id="burger-icon" className={navExpanded ? 'hidden' : 'nav-btn'} src={burgerIcon} alt="" />
           <img id="close-icon" className={navExpanded ? 'nav-btn' : 'hidden'} src={closeIcon} alt="" />
         </button>
         <nav className="first-nav">
-            <ul role='list' data-visible="false" id="primary-navigation" className="primary-navigation">
+            <ul ref={ref} role='list' data-visible="false" id="primary-navigation" className="primary-navigation">
                 <li><a href="#features-container">Features</a></li>
                 <li><a href="#download-section">Pricing</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a href="" className='btn accent-btn'>Login</a></li>
+                <li><a href="#" className='btn accent-btn'>Login</a></li>
             </ul>
         </nav>
     </header>
