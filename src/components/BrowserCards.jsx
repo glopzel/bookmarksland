9dd1b2516fa646aa5de25fd704df042a803c6333
glopzel <74939915+lozzel@users.noreply.github.com>
2023-02-20 21:31:04 -0300

    change id for class

--- src/components/BrowserCards.jsx
+++ src/components/BrowserCards.jsx
@@ -7,7 +7,7 @@ const BrowserCards = ({imgSrc, browser, version, cardId}) => {
         <h3 className='card-title'>Add to {browser}</h3>
         <p className='card-p'>Mimnimum version {version}</p>
         <div>
-          <img id='card-dots' src={dots} alt="" />
+          <img className='card-dots' src={dots} alt="" />
             <button className='btn primary-btn'>
                 Add &amp; Install Extension 
             </button>
